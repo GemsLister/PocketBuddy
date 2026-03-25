58ac8d0f101bece902fd20b7213c4dc2ed42ebd6
James <jameslopez@email.com>
2026-03-26 00:42:28 +0800

    style: add color details and apply font size and font family

--- screens/auth/LoginScreen.tsx
+++ screens/auth/LoginScreen.tsx
@@ -1,22 +1,60 @@
+import GoogleButton from "@/components/buttons/GoogleButton";
 import PrimaryButton from "@/components/buttons/PrimaryButton";
 import TextInputs from "@/components/inputs/TextInputs";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ms } from "react-native-size-matters";
+import { ms, vs } from "react-native-size-matters";
 export default function LoginScreen() {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
+    <SafeAreaView className="flex-1 justify-around items-center">
       <View>
-        <View>
+        <View className="items-center">
           <Text
             className="font-nunito-bold text-leaf"
-            style={{ fontSize: ms(40, 0.5) }}
+            style={{ fontSize: ms(50, 0.1) }}
           >
             PocketBuddy
           </Text>
+          <View className="flex-row gap-1" style={{ marginVertical: vs(10) }}>
+            <View>
+              <Text
+                className="font-nunito-semibold text-moss"
+                style={{ fontSize: ms(15, 0.5) }}
+              >
+                Don't have an account?
+              </Text>
+            </View>
+            {/* Link to registration */}
+            <View>
+              <Text
+                className="font-nunito-bold text-moss"
+                style={{ fontSize: ms(15, 0.5) }}
+              >
+                Register
+              </Text>
+            </View>
+          </View>
         </View>
         {/* Inputs */}
-        <View>
+        <View
+          className="w-screen"
+          style={{
+            marginTop: vs(10),
+            paddingHorizontal: ms(20, 0.7),
+            gap: vs(10),
+          }}
+        >
+          <View style={{ marginBottom: ms(10, 0.8), gap: ms(15, 0.5) }}>
+            <GoogleButton />
+            <View className="items-center">
+              <Text
+                className="font-nunito-semibold text-moss"
+                style={{ fontSize: ms(15, 0.5) }}
+              >
+                Or
+              </Text>
+            </View>
+          </View>
           <TextInputs placeholder="Email" secure={false} />
           <TextInputs placeholder="Password" secure={true} />
           <PrimaryButton text={"Login"} />

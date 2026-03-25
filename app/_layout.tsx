ca24991119e5f2270a9806a48003c10274ccf115
James <jameslopez@email.com>
2026-03-26 00:05:33 +0800

    feat: import Nunito fonts to add new font family

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import "@/global.css";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    "Nunito-Regular": require("@/assets/fonts/Nunito/Nunito-Regular.ttf"),
+    "Nunito-SemiBold": require("@/assets/fonts/Nunito/Nunito-SemiBold.ttf"),
+    "Nunito-Bold": require("@/assets/fonts/Nunito/Nunito-Bold.ttf"),
+  });
+  if (!fontsLoaded) return null;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }

9f2f242e370ef6635336544d9dbf374c0045717f
James <jameslopez@email.com>
2026-03-26 00:04:47 +0800

    feat: add screens in the content and add green color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.tsx",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
@@ -9,6 +13,13 @@ module.exports = {
         nunito: "Nunito-Regular",
         "nunito-semibold": "Nunito-SemiBold",
         "nunito-bold": "Nunito-Bold",
+      },
+      // Green Colors
+      colors: {
+        leaf: "#588157",
+        cream: "#dcd8cc",
+        beige: "#a0b089",
+        moss: "#385a41",
       },
     },
   },

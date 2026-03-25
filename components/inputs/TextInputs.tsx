e86c5778f44160979e243ed93384adebbab033cf
James <jameslopez@email.com>
2026-03-26 00:44:46 +0800

    style: apply colors and fonts  in inputs

--- components/inputs/TextInputs.tsx
+++ components/inputs/TextInputs.tsx
@@ -1,5 +1,5 @@
 import { TextInput, View } from "react-native";
-
+import { ms } from "react-native-size-matters";
 type TextInputsProps = {
   placeholder: string;
   secure: boolean;
@@ -8,7 +8,16 @@ type TextInputsProps = {
 export default function TextInputs({ placeholder, secure }: TextInputsProps) {
   return (
     <View>
-      <TextInput placeholder={placeholder} secureTextEntry={secure} />
+      <TextInput
+        className="font-nunito border border-slate-400"
+        style={{
+          fontSize: ms(15, 0.5),
+          padding: ms(10, 0.5),
+          borderRadius: ms(15, 0.3),
+        }}
+        placeholder={placeholder}
+        secureTextEntry={secure}
+      />
     </View>
   );
 }

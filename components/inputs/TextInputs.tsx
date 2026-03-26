0ab223b691a8f318562b94664e84efe04aba549d
James <jameslopez@email.com>
2026-03-27 01:16:21 +0800

    feat: apply react-native-paper library for floating placeholder effects

--- components/inputs/TextInputs.tsx
+++ components/inputs/TextInputs.tsx
@@ -1,4 +1,5 @@
-import { TextInput, View } from "react-native";
+import { View } from "react-native";
+import { TextInput } from "react-native-paper";
 import { ms } from "react-native-size-matters";
 type TextInputsProps = {
   placeholder: string;
@@ -9,14 +10,20 @@ export default function TextInputs({ placeholder, secure }: TextInputsProps) {
   return (
     <View>
       <TextInput
-        className="font-nunito border border-slate-400"
+        mode="outlined"
+        className="font-nunito"
         style={{
           fontSize: ms(15, 0.5),
-          padding: ms(10, 0.5),
           borderRadius: ms(15, 0.3),
         }}
-        placeholder={placeholder}
+        label={placeholder}
         secureTextEntry={secure}
+        outlineStyle={{
+          borderRadius: ms(13, 0.3),
+          borderWidth: 1, // Keeps the border thin
+        }}
+        outlineColor="#94a3b8" // slate-400 (Inactive color)
+        activeOutlineColor="#94a3b8"
       />
     </View>
   );

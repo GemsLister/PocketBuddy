bcfab843f70c1622ee7cd46383c16b7e0098e4d3
James <jameslopez@email.com>
2026-03-27 01:17:09 +0800

    fix: apply KeyboardAvoidingView to fix overlapping issues of input

--- screens/auth/LoginScreen.tsx
+++ screens/auth/LoginScreen.tsx
@@ -1,65 +1,76 @@
 import GoogleButton from "@/components/buttons/GoogleButton";
 import PrimaryButton from "@/components/buttons/PrimaryButton";
 import TextInputs from "@/components/inputs/TextInputs";
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { KeyboardAvoidingView, Platform, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ms, vs } from "react-native-size-matters";
 export default function LoginScreen() {
   return (
     <SafeAreaView className="flex-1 justify-around items-center">
-      <View>
-        <View className="items-center">
-          <Text
-            className="font-nunito-bold text-leaf"
-            style={{ fontSize: ms(50, 0.1) }}
+      <KeyboardAvoidingView
+        enabled={true}
+        behavior={Platform.OS === "android" ? "padding" : "height"}
+      >
+        <View>
+          <View className="items-center">
+            <Text
+              className="font-nunito-bold text-leaf"
+              style={{ fontSize: ms(50, 0.1) }}
+            >
+              PocketBuddy
+            </Text>
+            <View className="flex-row gap-1" style={{ marginVertical: vs(10) }}>
+              <View>
+                <Text
+                  className="font-nunito-semibold text-moss"
+                  style={{ fontSize: ms(15, 0.5) }}
+                >
+                  Don't have an account?
+                </Text>
+              </View>
+              {/* Link to registration */}
+              <View>
+                <Link href={"/(auth)/register"} asChild>
+                  <Text
+                    className="font-nunito-bold text-moss"
+                    style={{ fontSize: ms(15, 0.5) }}
+                  >
+                    Register
+                  </Text>
+                </Link>
+              </View>
+            </View>
+          </View>
+          {/* Inputs */}
+          <View
+            className="w-screen"
+            style={{
+              marginTop: vs(10),
+              paddingHorizontal: ms(20, 0.7),
+              gap: vs(10),
+            }}
           >
-            PocketBuddy
-          </Text>
-          <View className="flex-row gap-1" style={{ marginVertical: vs(10) }}>
-            <View>
-              <Text
-                className="font-nunito-semibold text-moss"
-                style={{ fontSize: ms(15, 0.5) }}
-              >
-                Don't have an account?
-              </Text>
+            <View style={{ marginBottom: ms(10, 0.8), gap: ms(15, 0.5) }}>
+              <GoogleButton />
+              <View className="items-center">
+                <Text
+                  className="font-nunito-semibold text-moss"
+                  style={{ fontSize: ms(15, 0.5) }}
+                >
+                  Or
+                </Text>
+              </View>
             </View>
-            {/* Link to registration */}
-            <View>
-              <Text
-                className="font-nunito-bold text-moss"
-                style={{ fontSize: ms(15, 0.5) }}
-              >
-                Register
-              </Text>
+
+            <View style={{ gap: vs(10), paddingBottom: vs(20) }}>
+              <TextInputs placeholder="Email" secure={false} />
+              <TextInputs placeholder="Password" secure={true} />
+              <PrimaryButton text={"Login"} />
             </View>
           </View>
         </View>
-        {/* Inputs */}
-        <View
-          className="w-screen"
-          style={{
-            marginTop: vs(10),
-            paddingHorizontal: ms(20, 0.7),
-            gap: vs(10),
-          }}
-        >
-          <View style={{ marginBottom: ms(10, 0.8), gap: ms(15, 0.5) }}>
-            <GoogleButton />
-            <View className="items-center">
-              <Text
-                className="font-nunito-semibold text-moss"
-                style={{ fontSize: ms(15, 0.5) }}
-              >
-                Or
-              </Text>
-            </View>
-          </View>
-          <TextInputs placeholder="Email" secure={false} />
-          <TextInputs placeholder="Password" secure={true} />
-          <PrimaryButton text={"Login"} />
-        </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }

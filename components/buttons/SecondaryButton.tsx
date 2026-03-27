7ff68940d902faddabd4041229557fc12f2978b9
James <jameslopez@email.com>
2026-03-28 01:19:09 +0800

    feat(SecondaryButton): add navigation support via expo-router Link

--- components/buttons/SecondaryButton.tsx
+++ components/buttons/SecondaryButton.tsx
@@ -1,30 +1,34 @@
+import { Href, Link } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import { ms } from "react-native-size-matters";
 
 type SecondaryButtonProps = {
   text: string;
+  link: Href;
 };
-export default function SecondaryButton({ text }: SecondaryButtonProps) {
+export default function SecondaryButton({ text, link }: SecondaryButtonProps) {
   return (
     <View>
-      <TouchableOpacity
-        className="flex-row justify-center items-center border-[1.5px] border-leaf"
-        activeOpacity={0.7}
-        style={{
-          padding: ms(10, 0.5),
-          borderRadius: ms(15, 0.3),
-          gap: ms(5, 0.3),
-        }}
-      >
-        <View>
-          <Text
-            className="text-moss font-nunito-semibold"
-            style={{ fontSize: ms(18, 0.5) }}
-          >
-            {text}
-          </Text>
-        </View>
-      </TouchableOpacity>
+      <Link href={link} asChild>
+        <TouchableOpacity
+          className="flex-row justify-center items-center border-[1.5px] border-leaf"
+          activeOpacity={0.7}
+          style={{
+            padding: ms(10, 0.5),
+            borderRadius: ms(15, 0.3),
+            gap: ms(5, 0.3),
+          }}
+        >
+          <View>
+            <Text
+              className="text-moss font-nunito-semibold"
+              style={{ fontSize: ms(18, 0.5) }}
+            >
+              {text}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }

1239674affef95f321280d37237b9f700e3d7031
James <jameslopez@email.com>
2026-03-28 01:47:27 +0800

    feat(PrimaryButton): add link prop and expo-router integration

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -1,26 +1,30 @@
+import { Href, Link } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import { ms } from "react-native-size-matters";
 type PrimaryButtonProps = {
   text: string;
+  link: Href;
 };
 
-export default function PrimaryButton({ text }: PrimaryButtonProps) {
+export default function PrimaryButton({ text, link }: PrimaryButtonProps) {
   return (
     <View>
-      <TouchableOpacity
-        className="bg-moss items-center"
-        activeOpacity={0.7}
-        style={{ padding: ms(10, 0.5), borderRadius: ms(15, 0.3) }}
-      >
-        <View>
-          <Text
-            className="text-white font-nunito-bold"
-            style={{ fontSize: ms(18, 0.5) }}
-          >
-            {text}
-          </Text>
-        </View>
-      </TouchableOpacity>
+      <Link href={link} asChild>
+        <TouchableOpacity
+          className="bg-moss items-center"
+          activeOpacity={0.7}
+          style={{ padding: ms(10, 0.5), borderRadius: ms(15, 0.3) }}
+        >
+          <View>
+            <Text
+              className="text-white font-nunito-bold"
+              style={{ fontSize: ms(18, 0.5) }}
+            >
+              {text}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }

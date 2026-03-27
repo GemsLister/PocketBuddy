3e658c0ee1ace586917147184c6239516a222982
James <jameslopez@email.com>
2026-03-28 00:34:50 +0800

    feat(components): add screenTitle and refine AuthContainer layout

--- components/container/AuthContainer.tsx
+++ components/container/AuthContainer.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { KeyboardAvoidingView, Platform, View } from "react-native";
+import { KeyboardAvoidingView, Platform, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ms, vs } from "react-native-size-matters";
 import PrimaryButton from "../buttons/PrimaryButton";
@@ -7,17 +7,30 @@ import PrimaryButton from "../buttons/PrimaryButton";
 type AuthContainerProps = {
   children: ReactNode;
   text: string;
+  screenTitle: string;
 };
-export default function AuthContainer({ children, text }: AuthContainerProps) {
+export default function AuthContainer({
+  children,
+  text,
+  screenTitle,
+}: AuthContainerProps) {
   return (
     <SafeAreaView
       className="flex-1 justify-between"
-      style={{ paddingTop: vs(15 * 3) }}
+      style={{ paddingTop: vs(15) }}
     >
       <KeyboardAvoidingView
         enabled={true}
         behavior={Platform.OS === "android" ? "padding" : "height"}
       >
+        <View>
+          <Text
+            className="font-nunito-bold text-moss"
+            style={{ fontSize: ms(23, 0.5) }}
+          >
+            {screenTitle}
+          </Text>
+        </View>
         <View
           className="w-screen"
           style={{
@@ -35,10 +48,14 @@ export default function AuthContainer({ children, text }: AuthContainerProps) {
             paddingHorizontal: ms(20, 0.7),
             gap: vs(10),
           }}
-        >
-          <PrimaryButton text={text} />
-        </View>
+        ></View>
       </KeyboardAvoidingView>
+      <View
+        className="w-screen"
+        style={{ paddingHorizontal: ms(20, 0.7), paddingBottom: vs(13 * 2) }}
+      >
+        <PrimaryButton text={text} />
+      </View>
     </SafeAreaView>
   );
 }

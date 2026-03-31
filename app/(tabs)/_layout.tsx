dda4db4124d3444f5f3276dee033db7688acf9e0
James <jameslopez@email.com>
2026-04-01 01:31:05 +0800

    feat(tabs): add new new tabs (charts and profile)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,13 +2,38 @@ import { Ionicons } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
 import { Tabs } from "expo-router";
 import { ms } from "react-native-size-matters";
+
 export default function TabRootLayout() {
+  const tabs: {
+    name: string;
+    title: string;
+    icon: keyof typeof Ionicons.glyphMap;
+  }[] = [
+    {
+      name: "records",
+      title: "Records",
+      icon: "newspaper-outline",
+    },
+    {
+      name: "charts",
+      title: "Charts",
+      icon: "pie-chart-outline",
+    },
+    {
+      name: "profile",
+      title: "Profile",
+      icon: "person-outline",
+    },
+  ];
+
   const [fontsLoaded] = useFonts({
     "Nunito-Regular": require("@/assets/fonts/Nunito/Nunito-Regular.ttf"),
     "Nunito-SemiBold": require("@/assets/fonts/Nunito/Nunito-SemiBold.ttf"),
     "Nunito-Bold": require("@/assets/fonts/Nunito/Nunito-Bold.ttf"),
   });
+
   if (!fontsLoaded) return null;
+
   return (
     <Tabs
       screenOptions={{
@@ -20,15 +45,18 @@ export default function TabRootLayout() {
         },
       }}
     >
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="home" size={ms(24, 0.5)} color={"#385a41"} />
-          ),
-        }}
-      />
+      {tabs.map((item, index) => (
+        <Tabs.Screen
+          key={index}
+          name={item.name}
+          options={{
+            title: item.title,
+            tabBarIcon: ({ color }) => (
+              <Ionicons name={item.icon} size={ms(24, 0.5)} color="#385a41" />
+            ),
+          }}
+        />
+      ))}
     </Tabs>
   );
 }

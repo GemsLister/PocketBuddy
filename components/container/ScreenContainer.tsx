07a61e1cd9cb1526655b0a75a9d8d60cbafeeccb
James <jameslopez@email.com>
2026-04-03 02:10:28 +0800

    feat: create reusable ScreenContainer that can be use in the Records, Charts, and Profile

---
+++ components/container/ScreenContainer.tsx
@@ -0,0 +1,20 @@
+import * as Button from "@/components/buttons/buttonsIndex";
+import type { ReactNode } from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ms } from "react-native-size-matters";
+
+type ScreenContainerProps = {
+  children: ReactNode;
+};
+
+export default function ScreenContainer({ children }: ScreenContainerProps) {
+  return (
+    <SafeAreaView className="flex-1 justify-between">
+      {children}
+      <View className="items-end" style={{ paddingHorizontal: ms(25, 0.8) }}>
+        <Button.AddButton />
+      </View>
+    </SafeAreaView>
+  );
+}

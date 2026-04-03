43e38cd7dde5a9f17d17c1335ff07e50bda98a3c
James <jameslopez@email.com>
2026-04-03 14:14:03 +0800

    ref: change AddScreen into IncomeScreen

---
+++ screens/tabs/IncomeScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function IncomeScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Add</Text>
+      </View>
+    </SafeAreaView>
+  );
+}

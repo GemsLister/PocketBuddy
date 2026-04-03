75a119fbf0224dfe606b4f2eef3e4bbd6a7ba53a
James <jameslopez@email.com>
2026-04-03 18:04:16 +0800

    feat: add title for transaction screens

--- app/(transaction)/_layout.tsx
+++ app/(transaction)/_layout.tsx
@@ -1,15 +1,28 @@
 import * as Button from "@/components/buttons/buttonsIndex";
 import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+import { ms } from "react-native-size-matters";
+
 export default function TransactionLayout() {
   return (
     <Stack screenOptions={{ headerShown: true }}>
       <Stack.Screen
         name="income"
-        options={({ navigation }) => ({
-          title: "",
+        options={{
+          headerTitle: () => (
+            <View>
+              <Text
+                className="font-nunito-bold text-moss"
+                style={{ fontSize: ms(20, 0.5) }}
+              >
+                Add
+              </Text>
+            </View>
+          ),
+          headerTitleAlign: "center",
           headerLeft: () => <Button.CancelButton />,
           headerRight: () => <Button.SaveButton />,
-        })}
+        }}
       />
     </Stack>
   );

b294c2c0092e306b1365bbf9d361c14e3bed9ab0
James <jameslopez@email.com>
2026-04-03 18:08:02 +0800

    style: edit button color into beige and add padding 10 in labels

--- components/buttons/SegmentedButtons.tsx
+++ components/buttons/SegmentedButtons.tsx
@@ -2,29 +2,47 @@ import { useState } from "react";
 import { View } from "react-native";
 import { SegmentedButtons } from "react-native-paper";
 import { ms } from "react-native-size-matters";
-
 export default function SegmentedButton() {
   const [value, setValue] = useState("income");
   return (
-    <View>
+    <View className="w-full items-center">
       <SegmentedButtons
         value={value}
         onValueChange={setValue}
         density="medium"
         theme={{
           colors: {
-            secondaryContainer: "#385a41", // Your leaf green
+            secondaryContainer: "#588157", // Your leaf green
             onSecondaryContainer: "white",
+          },
+          fonts: {
+            labelLarge: {
+              fontFamily: "Nunito-Bold",
+              fontSize: ms(15, 0.5),
+            },
           },
         }}
         buttons={[
-          { value: "expense", label: "Expense" },
-          { value: "income", label: "Income" },
-          { value: "transfer", label: "Transfer" },
+          {
+            value: "expense",
+            label: "Expense",
+            labelStyle: { padding: ms(10, 0.1) },
+          },
+          {
+            value: "income",
+            label: "Income",
+            labelStyle: { padding: ms(10, 0.1) },
+          },
+          {
+            value: "transfer",
+            label: "Transfer",
+            labelStyle: { padding: ms(10, 0.1) },
+          },
         ]}
         style={{
-          borderRadius: 10,
-          width: ms(200, 0.2),
+          alignItems: "center",
+          width: "90%",
+          borderRadius: 0,
         }}
       />
     </View>

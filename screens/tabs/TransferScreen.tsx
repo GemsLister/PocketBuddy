37c1f28766f8efa792e14e2a48aaa828e8b802c4
James <jameslopez@email.com>
2026-04-14 12:14:17 +0800

    test: create initial text "Transfer" to observe if moving to another screen works

---
+++ screens/tabs/TransferScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function TransferScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Transfer</Text>
+      </View>
+    </SafeAreaView>
+  );
+}

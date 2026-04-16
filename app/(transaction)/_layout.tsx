04cb18f31ed3fb5cc078ec3516a0301bfc790a6d
James <jameslopez@email.com>
2026-04-16 14:50:58 +0800

    feat: add mapping for income, expense, and transfer screens

--- app/(transaction)/_layout.tsx
+++ app/(transaction)/_layout.tsx
@@ -1,29 +1,35 @@
-import * as Button from "@/components/buttons/buttonsIndex";
+import * as Buttons from "@/components/buttons/buttonsIndex";
 import { Stack } from "expo-router";
 import { Text, View } from "react-native";
 import { ms } from "react-native-size-matters";
 
 export default function TransactionLayout() {
   return (
-    <Stack screenOptions={{ headerShown: true }}>
-      <Stack.Screen
-        name="income"
-        options={{
-          headerTitle: () => (
-            <View>
-              <Text
-                className="font-nunito-bold text-moss"
-                style={{ fontSize: ms(20, 0.5) }}
-              >
-                Add
-              </Text>
-            </View>
-          ),
-          headerTitleAlign: "center",
-          headerLeft: () => <Button.CancelButton />,
-          headerRight: () => <Button.SaveButton />,
-        }}
-      />
+    <Stack>
+      {[
+        { name: "income", title: "Income" },
+        { name: "expense", title: "Expense" },
+        { name: "transfer", title: "Transfer" },
+      ].map((items, index) => (
+        <Stack.Screen
+          key={index}
+          name={items.name}
+          options={{
+            headerTitle: () => (
+              <View>
+                <Text
+                  className="font-nunito-bold text-moss"
+                  style={{ fontSize: ms(18, 0.5) }}
+                >
+                  {items.title}
+                </Text>
+              </View>
+            ),
+            headerLeft: () => <Buttons.CancelButton />,
+            headerRight: () => <Buttons.SaveButton />,
+          }}
+        />
+      ))}
     </Stack>
   );
 }

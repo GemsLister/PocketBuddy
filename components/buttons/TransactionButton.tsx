6c0bcfbcecd2d6174216ff8e4b8668febea24d2b
James <jameslopez@email.com>
2026-04-16 18:33:44 +0800

    ref: apply usePathName for active buttons

--- components/buttons/TransactionButton.tsx
+++ components/buttons/TransactionButton.tsx
@@ -1,50 +1,57 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Link } from "expo-router";
-import { useState } from "react";
+import { Link, usePathname } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import { ms } from "react-native-size-matters";
 
 export default function TransactionButton() {
-  const [clicked, setClicked] = useState(false);
+  const pathName = usePathname();
+
   const transactionIcons = [
     {
-      icon: <Ionicons name="receipt-outline" size={ms(35, 0.5)} />,
+      icon: "receipt-outline",
       path: "/(transaction)/expense",
       name: "Expense",
     },
     {
-      icon: <Ionicons name="cash-outline" size={ms(35, 0.5)} />,
+      icon: "cash-outline",
       path: "/(transaction)/income",
       name: "Income",
     },
     {
-      icon: <Ionicons name="card-outline" size={ms(35, 0.5)} />,
+      icon: "card-outline",
       path: "/(transaction)/transfer",
       name: "Transfer",
     },
   ];
+
   return (
     <View className="gap-4 flex-row justify-center">
-      {transactionIcons.map((icons, index) => (
-        <Link href={icons.path as any} asChild key={index}>
-          <TouchableOpacity
-            // className=`$(border-[1.5px] border-moss rounded-lg justify-center items-center`
-            className={`${!clicked ? "border border-moss bg-white" : "bg-leaf"} rounded-lg justify-center items-center`}
-            style={{ padding: ms(25, 0.5) }}
-            onPressIn={() => setClicked(true)}
-            onPressOut={() => setClicked(false)}
-          >
-            {icons.icon}
-            {/* className="font-nunito-semibold text-moss" */}
-            <Text
-              className={`${!clicked ? "text-moss" : "text-white"} font-nunito-bold`}
-              style={{ fontSize: ms(14, 0.5) }}
+      {transactionIcons.map((item, index) => {
+        const isActive = pathName.endsWith(item.name.toLowerCase());
+
+        return (
+          <Link href={item.path as any} asChild key={index}>
+            <TouchableOpacity
+              className={`${
+                isActive ? "bg-leaf" : "bg-white border border-moss"
+              } rounded-3xl justify-center items-center`}
+              style={{ padding: ms(25, 0.5) }}
             >
-              {icons.name}
-            </Text>
-          </TouchableOpacity>
-        </Link>
-      ))}
+              <Ionicons
+                name={item.icon as any}
+                size={ms(40, 0.5)}
+                color={isActive ? "white" : "#4A5D4E"}
+              />
+              <Text
+                className={`${isActive ? "text-white" : "text-moss"} font-nunito-semibold`}
+                style={{ fontSize: ms(14, 0.5) }}
+              >
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          </Link>
+        );
+      })}
     </View>
   );
 }

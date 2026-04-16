eb0b6db8c6c903707c128d1c8a48d587daced020
James <jameslopez@email.com>
2026-04-16 17:07:54 +0800

    ref: change TransferScreen location

--- app/(transaction)/transfer.tsx
+++ app/(transaction)/transfer.tsx
@@ -1,4 +1,4 @@
-import TransferScreen from "@/screens/tabs/TransferScreen";
+import TransferScreen from "@/screens/tabs/transaction/TransferScreen";
 
 export default function TransferLayout() {
   return <TransferScreen />;

7a8de1a07a440d8e45e8c8e23dddd43bd9d83096
James <jameslopez@email.com>
2026-04-16 18:31:36 +0800

    ref: change the route go back to records

--- components/buttons/CancelButton.tsx
+++ components/buttons/CancelButton.tsx
@@ -1,20 +1,18 @@
 import { Ionicons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
+import { Link } from "expo-router";
 import { TouchableOpacity, View } from "react-native";
 import { ms } from "react-native-size-matters";
 export default function SaveButton() {
-  const router = useRouter();
   return (
     <View>
-      <TouchableOpacity
-        className="flex-row items-center"
-        onPress={() => router.back()}
-      >
-        <Ionicons
-          name="chevron-back-outline"
-          size={ms(24, 0.5)}
-          color="#385a41"
-        />
+      <TouchableOpacity className="flex-row items-center">
+        <Link href={"/(tabs)/records"}>
+          <Ionicons
+            name="chevron-back-outline"
+            size={ms(24, 0.5)}
+            color="#385a41"
+          />
+        </Link>
       </TouchableOpacity>
     </View>
   );

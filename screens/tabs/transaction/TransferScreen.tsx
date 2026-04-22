109345cc12688b418b18d1c6b0a7cce5cc299d06
James <jameslopez@email.com>
2026-04-23 02:52:56 +0800

    test: add initial transferIcons to test transfer screen

--- screens/tabs/transaction/TransferScreen.tsx
+++ screens/tabs/transaction/TransferScreen.tsx
@@ -1,9 +1,43 @@
 import * as Buttons from "@/components/buttons/buttonsIndex";
+import CategoriesContainer from "@/components/container/CategoriesContainer";
+import { Ionicons } from "@expo/vector-icons";
+import { ComponentProps } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+type IoniconsName = ComponentProps<typeof Ionicons>["name"];
+
 export default function TransferScreen() {
+  const transferIcons: { name: string; icon: IoniconsName }[] = [
+    {
+      name: "Salary",
+      icon: "cash-outline",
+    },
+    {
+      name: "Freelance",
+      icon: "briefcase-outline",
+    },
+    {
+      name: "Business",
+      icon: "trending-up-outline",
+    },
+    {
+      name: "Allowance",
+      icon: "wallet-outline",
+    },
+    {
+      name: "Gifts",
+      icon: "gift-outline",
+    },
+    {
+      name: "Others",
+      icon: "add-outline",
+    },
+  ];
   return (
     <SafeAreaView>
       <Buttons.TransactionButton />
+      {/* Categories */}
+      <CategoriesContainer icons={transferIcons} view="Transfer" />
     </SafeAreaView>
   );
 }

48d6c411dcb76ec8d7d0746803f4abf5bad438c3
James <jameslopez@email.com>
2026-04-25 02:46:44 +0800

    style: change the category button bg-color

--- components/container/CategoriesContainer.tsx
+++ components/container/CategoriesContainer.tsx
@@ -1,7 +1,8 @@
+import * as Buttons from "@/components/buttons/buttonsIndex";
 import { Ionicons } from "@expo/vector-icons";
 import { ComponentProps, ReactNode, useState } from "react";
 import { Pressable, Text, TextInput, View } from "react-native";
-import { ms } from "react-native-size-matters";
+import { ms, vs } from "react-native-size-matters";
 type IoniconsName = ComponentProps<typeof Ionicons>["name"];
 
 type CategoryItem = {
@@ -24,17 +25,20 @@ export default function CategoriesContainer({ icons, view }: CategoriesProps) {
       style={{
         paddingVertical: ms(17, 0.5),
         paddingHorizontal: ms(15, 0.5),
-        gap: ms(20, 0.3),
+        gap: ms(10, 0.3),
       }}
     >
       <Text
         className="font-nunito-bold text-moss"
-        style={{ fontSize: ms(25, 0.5) }}
+        style={{ fontSize: ms(20, 0.5) }}
       >
         Categories
       </Text>
 
-      <View className="flex-row flex-wrap" style={{ gap: ms(15, 0.6) }}>
+      <View
+        className="flex-row flex-wrap items-center rounded-3xl"
+        style={{ gap: ms(20, 0.8) }}
+      >
         {icons.map((item, index) => {
           const isSelected = selectedCategory === item.name;
           return (
@@ -45,70 +49,87 @@ export default function CategoriesContainer({ icons, view }: CategoriesProps) {
                 setAmount("");
                 setNote("");
               }}
-              className={`flex items-center justify-center gap-3 rounded-3xl border ${isSelected ? "bg-moss border-moss" : "bg-moss/10 border-moss/20 active:bg-moss/20"}`}
+              className={`rounded-3xl justify-center gap-3 ${isSelected ? "bg-leaf" : "bg-beige"}`}
               style={{
-                height: ms(100, 0.7),
-                width: ms(100, 0.7),
-                padding: ms(8, 0.5),
+                width: ms(100, 0.2),
+                aspectRatio: 1,
               }}
             >
-              {typeof item.icon === "string" ? (
-                <Ionicons
-                  name={item.icon as IoniconsName}
-                  size={ms(35, 0.5)}
-                  color={isSelected ? "#ffffff" : "#385a41"}
-                />
-              ) : (
-                item.icon
-              )}
-              <Text
-                className={`font-nunito-semibold ${isSelected ? "text-white" : "text-moss"}`}
-                style={{ fontSize: ms(15, 0.7) }}
+              <View
+                className="items-center justify-center"
+                style={{ transform: [{ scale: 1 }] }}
               >
-                {item.name}
-              </Text>
+                <View className="bg-beige p-3 rounded-full">
+                  {typeof item.icon === "string" ? (
+                    <Ionicons
+                      name={item.icon as IoniconsName}
+                      size={ms(25, 0.5)}
+                      color={isSelected ? "#ffffff" : "#385a41"}
+                    />
+                  ) : (
+                    item.icon
+                  )}
+                </View>
+
+                <Text
+                  className={`font-nunito-bold ${isSelected ? "text-white" : "text-moss"}`}
+                  style={{ fontSize: vs(10) }}
+                >
+                  {item.name}
+                </Text>
+              </View>
             </Pressable>
           );
         })}
       </View>
 
       {selectedCategory && (
-        <View style={{ gap: ms(12, 0.5) }}>
-          <Text
-            className="font-nunito-bold text-moss"
-            style={{ fontSize: ms(18, 0.5) }}
-          >
-            {selectedCategory}
-          </Text>
+        <View style={{ gap: ms(12, 0.5), marginTop: vs(10) }}>
+          <View className="flex-row justify-between">
+            <Text
+              className="font-nunito-bold text-moss"
+              style={{ fontSize: vs(15) }}
+            >
+              {selectedCategory}
+            </Text>
+            <Pressable
+              onPress={() => {
+                setSelectedCategory(null);
+                setAmount("");
+                setNote("");
+              }}
+            >
+              <Ionicons name="close-outline" size={vs(20)} color={"#385a41"} />
+            </Pressable>
+          </View>
 
-          <TextInput
-            className="font-nunito text-moss border border-slate-400"
-            style={{
-              fontSize: ms(16, 0.5),
-              borderRadius: ms(13, 0.3),
-              paddingVertical: ms(10, 0.5),
-              paddingHorizontal: ms(14, 0.5),
-            }}
-            placeholder="Amount"
-            placeholderTextColor="#94a3b8"
-            keyboardType="decimal-pad"
-            value={amount}
-            onChangeText={setAmount}
-          />
-
-          <TextInput
-            className="font-nunito text-moss border border-slate-400"
-            style={{
-              fontSize: ms(16, 0.5),
-              borderRadius: ms(13, 0.3),
-              paddingVertical: ms(10, 0.5),
-              paddingHorizontal: ms(14, 0.5),
-            }}
-            placeholder="Note (optional)"
-            placeholderTextColor="#94a3b8"
-            value={note}
-            onChangeText={setNote}
-          />
+          {[
+            {
+              text: "Amount",
+              onChange: setAmount,
+            },
+            {
+              text: "Note (Optional)",
+              onChange: setNote,
+            },
+          ].map((items, index) => (
+            <TextInput
+              key={index}
+              className="font-nunito text-moss border border-slate-400"
+              style={{
+                fontSize: ms(16, 0.5),
+                borderRadius: ms(13, 0.3),
+                paddingVertical: ms(10, 0.5),
+                paddingHorizontal: ms(14, 0.5),
+              }}
+              placeholder={items.text}
+              placeholderTextColor="#94a3b8"
+              keyboardType="decimal-pad"
+              value={amount}
+              onChangeText={items.onChange}
+            />
+          ))}
+          <Buttons.PrimaryButton text="Ok" link={"/"} />
         </View>
       )}
     </View>

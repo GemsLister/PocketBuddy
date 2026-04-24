7fb32efc33d5da599bb24e17b3be84a30f019dad
James <jameslopez@email.com>
2026-04-24 12:10:28 +0800

    ref: change ExpenseIcon as InvestmentIcon and IncomeIcon as SavingsIcon

--- assets/icons/iconsIndex.ts
+++ assets/icons/iconsIndex.ts
@@ -1,4 +1,4 @@
-export { default as ExpenseIcon } from "./ExpenseIcon";
-export { default as IncomeIcon } from "./IncomeIcon";
+export { default as InvestmentIcon } from "./InvestmentIcon";
+export { default as SavingsIcon } from "./SavingsIcon";
 export { default as TransferIcon } from "./TransferIcon";
 

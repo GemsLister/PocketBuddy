7808bb65a3eed6e65cee7f185f7814fb8b6d8a7f
James <jameslopez@email.com>
2026-04-25 02:48:32 +0800

    style: edit border weight into 1

--- components/buttons/GoogleButton.tsx
+++ components/buttons/GoogleButton.tsx
@@ -5,7 +5,7 @@ export default function GoogleButton() {
   return (
     <View>
       <TouchableOpacity
-        className="flex-row justify-center items-center border-[1.5px] border-leaf"
+        className="flex-row justify-center items-center border border-leaf"
         activeOpacity={0.7}
         style={{
           padding: ms(10, 0.5),

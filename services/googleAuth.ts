b2be78e5e6f9a605c9b1358fe948c7b5e4de3421
James <jameslopez@email.com>
2026-04-27 01:46:52 +0800

    test: make offlineAccess true to test if google login works

--- services/googleAuth.ts
+++ services/googleAuth.ts
@@ -4,7 +4,7 @@ import { GoogleSignin } from "@react-native-google-signin/google-signin";
 export const handleGoogleLogin = async () => {
   GoogleSignin.configure({
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
-    // offlineAccess: true,
+    offlineAccess: true,
     // forceCodeForRefreshToken: true,
   });
   try {

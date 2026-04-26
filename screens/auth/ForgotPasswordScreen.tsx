951d786f690f845e09c613b79749542f54c67715
James <jameslopez@email.com>
2026-04-26 23:32:32 +0800

    ref: remove PrimaryButton and pass the handleSignUp in AuthContainer

--- screens/auth/ForgotPasswordScreen.tsx
+++ screens/auth/ForgotPasswordScreen.tsx
@@ -1,14 +1,23 @@
 import AuthContainer from "@/components/container/AuthContainer";
 import TextInputs from "@/components/inputs/TextInputs";
+import { useForgotPassword } from "@/hooks/auth/useForgotPassword";
+import { useState } from "react";
 export default function ForgotPasswordScreen() {
+  const [email, setEmail] = useState("");
+  const { handleForgotPassword } = useForgotPassword();
   return (
     <AuthContainer
       label="Continue"
       screenTitle="Recover your account"
       text="Enter your email address"
-      link={"/"}
+      onPress={() => handleForgotPassword(email)}
     >
-      <TextInputs placeholder="Email" secure={false} />
+      <TextInputs
+        placeholder="Email"
+        secure={false}
+        value={email}
+        onChangeText={setEmail}
+      />
     </AuthContainer>
   );
 }

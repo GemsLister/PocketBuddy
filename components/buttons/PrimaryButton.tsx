6c87993883d0ee02ce8d4118a398f704bde335c2
James <jameslopez@email.com>
2026-04-26 22:22:01 +0800

    ref: remove link props

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -1,30 +1,35 @@
-import { Href, Link } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import { ms } from "react-native-size-matters";
 type PrimaryButtonProps = {
   text: string;
-  link: Href;
+  // link: Href;
+  onPress: () => void;
 };
 
-export default function PrimaryButton({ text, link }: PrimaryButtonProps) {
+export default function PrimaryButton({
+  text,
+  // link,
+  onPress,
+}: PrimaryButtonProps) {
   return (
     <View>
-      <Link href={link} asChild>
-        <TouchableOpacity
-          className="bg-moss items-center"
-          activeOpacity={0.7}
-          style={{ padding: ms(10, 0.5), borderRadius: ms(15, 0.3) }}
-        >
-          <View>
-            <Text
-              className="text-white font-nunito-semibold"
-              style={{ fontSize: ms(18, 0.5) }}
-            >
-              {text}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </Link>
+      {/* <Link href={link} asChild> */}
+      <TouchableOpacity
+        className="bg-moss items-center"
+        activeOpacity={0.7}
+        style={{ padding: ms(10, 0.5), borderRadius: ms(15, 0.3) }}
+        onPress={onPress}
+      >
+        <View>
+          <Text
+            className="text-white font-nunito-semibold"
+            style={{ fontSize: ms(18, 0.5) }}
+          >
+            {text}
+          </Text>
+        </View>
+      </TouchableOpacity>
+      {/* </Link> */}
     </View>
   );
 }

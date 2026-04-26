e95fe92578d539c3f4d768108e3bcff4ecb4a8e8
James <jameslopez@email.com>
2026-04-26 22:25:38 +0800

    feat: apply useState hook for email and password

--- screens/auth/RegisterScreen.tsx
+++ screens/auth/RegisterScreen.tsx
@@ -1,8 +1,15 @@
+import PrimaryButton from "@/components/buttons/PrimaryButton";
 import AuthContainer from "@/components/container/AuthContainer";
 import TextInputs from "@/components/inputs/TextInputs";
+import { useSignUp } from "@/hooks/auth/useSignUp";
+import { useState } from "react";
 import { View } from "react-native";
 import { vs } from "react-native-size-matters";
+
 export default function RegisterScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { handleSignUp } = useSignUp();
   return (
     <AuthContainer
       label="Create Account"
@@ -11,9 +18,23 @@ export default function RegisterScreen() {
       link={"/"}
     >
       <View style={{ gap: vs(10) }}>
-        <TextInputs placeholder="Name" secure={false} />
-        <TextInputs placeholder="Email" secure={false} />
-        <TextInputs placeholder="Password" secure={true} />
+        {/* <TextInputs placeholder="Name" secure={false} /> */}
+        <TextInputs
+          placeholder="Email"
+          secure={false}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInputs
+          placeholder="Password"
+          secure={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <PrimaryButton
+          text="Register"
+          onPress={() => handleSignUp(email, password)}
+        />
       </View>
     </AuthContainer>
   );

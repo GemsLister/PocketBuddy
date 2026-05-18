917770d75640b7cfd66461af261301fce6a7906a
James Lester Lopez <jameslester.l.lopez@gmail.com>
2026-05-18 11:54:14 +0800

    feat(auth): integrate loading state into RegisterScreen

--- screens/auth/RegisterScreen.tsx
+++ screens/auth/RegisterScreen.tsx
@@ -9,13 +9,14 @@ export default function RegisterScreen() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { handleSignUp } = useSignUp();
+  const { handleSignUp, loading, error, success } = useSignUp();
   return (
     <AuthContainer
       label="Create Account"
       screenTitle="Let's Get Started!"
       text="Enter your personal details"
       onPress={() => handleSignUp(username, email, password)}
+      loading={loading}
     >
       <View style={{ gap: vs(10) }}>
         <TextInputs
